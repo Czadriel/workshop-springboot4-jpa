c2cfa018e23b237b652e6a5e847bc94cf31fb3d6
Adriel Cezar <adrielcezar022@gmail.com>
2026-05-08 11:55:04 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -7,7 +7,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import java.sql.Array;
 import java.util.Arrays;
 
 @Configuration
